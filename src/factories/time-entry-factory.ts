d769c04ee536c7515ce6ec0821f90e3ecf91157d
John Need <john.need@inulabs.tech>
2026-01-03 10:26:58 -0500

    fix: leading zero

--- src/factories/time-entry-factory.ts
+++ src/factories/time-entry-factory.ts
@@ -2,10 +2,11 @@ import toIso from "@/utils/to-iso";
 import generateUUID from "@/utils/generate-uuid";
 
 const timeEntryFactory = (data: Partial<TimeEntry> = {}): TimeEntry => {
+ const minutes = Number(data.minutes);
   return {
     id: typeof data.id === "string" ? data.id : generateUUID(),
     activityId: typeof data.activityId === "string" ? data.activityId : "",
-    minutes: typeof data.minutes === "number" ? data.minutes : 0,
+    minutes: isNaN(minutes) || minutes < 0 ? 0 : Math.round(minutes),
     notes: typeof data.notes === "string" ? data.notes : "",
     created: toIso(data.created),
     updated: toIso(data.updated)
